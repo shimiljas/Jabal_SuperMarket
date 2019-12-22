f6e79608ba7ec0a71c0990d4dc5c4912899e1524
shimil <kj@1ech.us>
2019-12-22 14:01:37 +0530

    production

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/ping', (req, res) => {
+    return res.send('pong')
+})
+
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build/index.html'), function (err) {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
+
+app.listen(8086, () => {
+    console.log('listening on PORT 8086')
+})
