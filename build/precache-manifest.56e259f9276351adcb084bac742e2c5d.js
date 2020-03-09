f7b53692fb116b082a58ecbba964925c34e3389b
shimil <kj@1ech.us>
2020-03-09 13:20:11 +0530

    inlcude build

---
+++ build/precache-manifest.56e259f9276351adcb084bac742e2c5d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8806c37e491cc2f7ac6c",
+    "url": "/static/css/main.a0e968ab.chunk.css"
+  },
+  {
+    "revision": "8806c37e491cc2f7ac6c",
+    "url": "/static/js/main.8806c37e.chunk.js"
+  },
+  {
+    "revision": "b6de3c1a9121c14fd4ae",
+    "url": "/static/js/runtime~main.b6de3c1a.js"
+  },
+  {
+    "revision": "d221524001b814000579",
+    "url": "/static/css/2.208421d4.chunk.css"
+  },
+  {
+    "revision": "d221524001b814000579",
+    "url": "/static/js/2.d2215240.chunk.js"
+  },
+  {
+    "revision": "537dd20aca5a2ef744fcca3701994153",
+    "url": "/static/media/404.537dd20a.png"
+  },
+  {
+    "revision": "b5761ff80081a8561ef325d920223815",
+    "url": "/static/media/logo_light.b5761ff8.png"
+  },
+  {
+    "revision": "cd3f84e9fabfbe22f8c63edef3e769e7",
+    "url": "/static/media/bg_404.cd3f84e9.png"
+  },
+  {
+    "revision": "48ab7481741026556f3faf6c43b18492",
+    "url": "/static/media/logo_dark.48ab7481.svg"
+  },
+  {
+    "revision": "a6d5a2ccb45f799c73254706fc98da07",
+    "url": "/index.html"
+  }
+]);
